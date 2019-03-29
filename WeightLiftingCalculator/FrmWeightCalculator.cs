aaf2c048b900becaf24a82a49db6e404b7b1ebe1
Isaac Burke <isaac.burke1@outlook.com>
2019-03-28 18:49:04 -0700

    Created the Maxes and Weight Calculator Forms

--- WeightLiftingCalculator/FrmWeightCalculator.cs
+++ WeightLiftingCalculator/FrmWeightCalculator.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WeightLiftingCalculator
 {
-    public partial class Form1 : Form
+    public partial class FrmWeightCalculator : Form
     {
-        public Form1()
+        public FrmWeightCalculator()
         {
             InitializeComponent();
         }
--- WeightLiftingCalculator/Program.cs
+++ WeightLiftingCalculator/Program.cs
@@ -16,7 +16,8 @@ namespace WeightLiftingCalculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmMaxes());
+            
         }
     }
 }
